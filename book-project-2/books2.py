355fcf2404c2a627853f1a3466881091e4ec24b6
Mattrey Kumar Rana <mrana23@myseneca.ca>
2025-12-18 14:32:04 -0500

    Increment ID in post request

--- book-project-2/books2.py
+++ book-project-2/books2.py
@@ -1,33 +1,49 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-class Book:
+class Book(BaseModel):
     id: int
     title: str
     author: str
     description: str
     rating: int
 
-    def __init__(self, book_id, title, author, description, rating):
-        self.id = book_id
-        self.title = title
-        self.author = author
-        self.description = description
-        self.rating = rating
+class BookRequest(BaseModel):
+    id: int = Field(gt=0)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=3)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(ge=1, le=5)
 
 BOOKS = [
-    Book(1, "Computer Science", 'codingwithroby', 'A very nice book!', 5),
-    Book(2, "FastAPI", 'codingwithroby', 'A great book!', 5),
-    Book(3, "Computer 101", 'codingwithroby', 'Amazing book!', 5),
-    Book(4, "Title 1", 'author 1', 'Bad book!', 1),
-    Book(5, "Title 2", 'author 2', 'Okayish!', 3),
+    Book(id=1, title="Computer Science", author="codingwithroby", description="A very nice book!", rating=5),
+    Book(id=2, title="FastAPI", author="codingwithroby", description="A great book!", rating=5),
+    Book(id=3, title="Computer 101", author="codingwithroby", description="Amazing book!", rating=5),
+    Book(id=4, title="Title 1", author="author 1", description="Bad book!", rating=1),
+    Book(id=5, title="Title 2", author="author 2", description="Okayish!", rating=3),
 ]
 
 @app.get("/")
 async def root():
-    return "Book2 Project Started"
+    return {"message": "Book2 Project Started"}
 
 @app.get("/books")
 async def get_books():
     return BOOKS
+
+@app.post("/create_book")
+async def create_book(book: BookRequest):
+    new_book = Book(**book.model_dump())
+    BOOKS.append(increment_id(new_book))
+    return new_book
+
+
+def increment_id(book:Book):
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 0
+
+    return book
