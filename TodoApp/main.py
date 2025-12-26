58125960b7298570f90b8ea859aa04322af13ed0
Mattrey Kumar Rana <mrana23@myseneca.ca>
2025-12-26 14:47:52 -0500

    GET Request added to TODO Project

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,27 @@
 # Main Python file for the Todos App
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing import Annotated
 import models
-from database import engine
+from models import Todos
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# GET Request
+@app.get("/")
+async def read_all_todos(db: db_dependency):
+    return db.query(Todos).all()
