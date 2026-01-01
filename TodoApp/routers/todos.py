30393caea1b3e109c744aec44c77768409db108f
Mattrey Kumar Rana <mrana23@myseneca.ca>
2026-01-01 18:51:24 -0500

    Authenticated DELETE  Request added to the Todo Project

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -50,7 +50,7 @@ async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Pat
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=401, detail="Not Authenticated")
 
     todo_model = Todos(**todo_request.model_dump(),
                        owner_id = user.get('id'))
@@ -63,7 +63,7 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_request: To
 async def update_todo(user: user_dependency, db: db_dependency, todo_request : TodoRequest ,todo_id: int = Path(gt = 0)):
 
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=401, detail="Not Authenticated")
 
     todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
 
@@ -80,11 +80,15 @@ async def update_todo(user: user_dependency, db: db_dependency, todo_request : T
 
 # DELETE Request
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_todo(db: db_dependency, todo_id: int = Path(gt = 0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def delete_todo(user: user_dependency, db : db_dependency, todo_id: int = Path(gt = 0)):
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="Not Authenticated")
+
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
 
     if todo_model is None:
         raise HTTPException(status_code=404, detail="Todo not found.")
 
-    db.delete(todo_model)
+    db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).delete()
     db.commit()
